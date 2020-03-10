4e0fcc40f2c9aa025676c95af36bfcb88e08b178
ASAA19971a <abel.2833@hotmail.com>
2020-03-10 11:06:53 -0500

    Update Clase1.java
    se declararon variables

--- Clase1/src/clase1/Clase1.java
+++ Clase1/src/clase1/Clase1.java
@@ -4,16 +4,27 @@
  * and open the template in the editor.
  */
 package clase1;
-
+//package y nombre de paquete
 /**
  *
  * @author ABEL
  */
 public class Clase1 {
-
+// public class nombre clase
     /**
      * @param args the command line arguments
      */
+    //entero -32768 a 32767
+    int numeroEntero = 5;
+    double numeroDouble = 6.89;
+    char letra = 'u';
+    String cadena = "Hola Mundo";
+    //String nombre variable y el valor entre comillas dobles
+    boolean buleano = false;
+    buleano = true;
+    boolean buleano2 = false;
+    
+    
     public static void main(String[] args) {
         // TODO code application logic hHLLISere
         // comentario una linea
