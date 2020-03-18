d1bd1d8b279f211fc2d2fb388f1d9b7d9317b9cc
ASAA19971a <abel.2833@hotmail.com>
2020-03-18 10:54:38 -0500

    Update Arreglos.java
    imprimir arreglos mediante for

--- Arreglos/src/arreglos/Arreglos.java
+++ Arreglos/src/arreglos/Arreglos.java
@@ -19,14 +19,26 @@ public class Arreglos {
         
         int arregloNumero[] = new int[]{1,25,9,8,15};
         
-        for(int i=0; i<5; i++){
-            System.out.println(arregloNumero[i]);
+        for(int i=0; i<arregloNumero.length; i++){
+            System.out.println("Valor indice "+ i + "  Valor arreglo  "+arregloNumero[i]);
         }
         
         
-        String arregloLetras[] = new String[]{"a","b","c"};
+        String arregloLetras[] = new String[]{"a","x","z"};
         System.out.println("ARREGLO LETRAS");
         System.out.println(arregloLetras[2]);
+        
+        for(int i=0; i<arregloLetras.length; i++){
+            
+            if(arregloLetras[i]=="x"){
+                System.out.println("es verdadero es x");
+            }else{
+                 System.out.println("es falso");
+            }
+            System.out.println("Valor indice "+ i + "  Valor arreglo  "+arregloLetras[i]);
+        }
+        System.out.println("TAMAÑO DE ARREGLOS");
+        System.out.println(arregloNumero.length);
         
         //final main
     }
